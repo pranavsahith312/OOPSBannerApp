25fa9c33c7a870c622293a1dad607eb01e3310bd
pranavsahith312 <pranavsahith.312@gmail.com>
2026-03-18 08:55:29 +0530

    Add files via upload

---
+++ OOPSBannerAppUC4.java
@@ -0,0 +1,17 @@
+public class OOPSBannerAppUC4 {
+    public static void main(String[] args) {
+        // Store banner lines in a String array
+        String[] bannerLines = {
+            "****************************",
+            "*                          *",
+            "*   Welcome to OOPS App    *",
+            "*                          *",
+            "****************************"
+        };
+        
+        // Use a loop to print all banner lines
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
+    }
+}
